f05cdd733d0f86fee8363c09e3b0f86815e6c6f6
Fran Agulto <francis.agulto@wpengine.com>
2025-07-24 13:01:46 -0500

    added config updates

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
+  image: {
+    domains: [
+      new URL(process.env.NUXT_PUBLIC_WORDPRESS_URL || "").hostname,
+    ].filter(Boolean),
+    quality: 80,
+    format: ["webp", "jpg", "png"],
+  },
+  app: {
+    head: {
+      title: "Nuxt headlesswp eCommerce",
+      meta: [{ name: "description", content: "Nuxt headlesswp eCommerce" }],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
+  runtimeConfig: {
+    public: {
+      wordpressUrl: "",
+      smartSearchUrl: "",
+      smartSearchToken: "",
+    },
+  },
+});
